98b033d1100ad2b7f894219485ba41f1ff006880
lloyddewit <stephen.lloyd03@gmail.com>
2021-03-24 17:15:26 +0100

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TranslateWinForm")>
+<Assembly: AssemblyDescription("Translates WinForm text (e.g. text in forms and menu items) into a different language (e.g. from English to French)")>
+<Assembly: AssemblyCompany("IDEMS International")>
+<Assembly: AssemblyProduct("TranslateWinForm")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6eaa2813-f88a-484f-ab18-644d1e8bbb66")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
+<Assembly: NeutralResourcesLanguage("en")>
---
+++ clsTranslateWinForm.vb
@@ -0,0 +1,109 @@
+﻿' IDEMS International
+' Copyright (C) 2021
+'
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License 
+' along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Imports System.Data.SQLite
+Imports System.Windows.Forms
+
+Public Class clsTranslateWinForm
+    Public Shared Sub translateForm(clsForm As Form, strDataSource As String, Optional strLanguage As String = "")
+        'connect to the SQLite database that contains the translations
+        Dim clsBuilder As New SQLiteConnectionStringBuilder With {
+            .FailIfMissing = True,
+            .DataSource = strDataSource
+        }
+        Using clsConnection As New SQLiteConnection(clsBuilder.ConnectionString)
+            clsConnection.Open()
+            Using clsCommand As New SQLiteCommand(clsConnection)
+
+                'get all translations for the specified form and language
+                clsCommand.CommandText = "SELECT control_name, translation FROM form_controls, translations WHERE form_name = """ & clsForm.Name &
+                                         """ AND language_code = """ & strLanguage & """ And form_controls.id_text = translations.id_text"
+                Dim clsReader As SQLiteDataReader = clsCommand.ExecuteReader()
+                Using clsReader
+
+                    'for each translation row
+                    While (clsReader.Read())
+
+                        'translate the control's text to the new language
+                        Dim strControlName As String = clsReader.GetString(0)
+                        Dim strTranslation As String = clsReader.GetString(1)
+                        CallByName(clsForm.Controls(strControlName), "Text", CallType.Set, strTranslation)
+
+                    End While
+                End Using
+            End Using
+            clsConnection.Close()
+        End Using
+    End Sub
+
+    Public Shared Sub translateMenuItems(tsCollection As ToolStripItemCollection, ctrParent As Control, strDataSource As String, Optional strLanguage As String = "")
+
+        Dim dctMenuItems As Dictionary(Of String, ToolStripMenuItem) = GetDctMenuItems(tsCollection)
+
+        'connect to the SQLite database that contains the translations
+        Dim clsBuilder As New SQLiteConnectionStringBuilder With {
+            .FailIfMissing = True,
+            .DataSource = strDataSource
+        }
+        Using clsConnection As New SQLiteConnection(clsBuilder.ConnectionString)
+            clsConnection.Open()
+            Using clsCommand As New SQLiteCommand(clsConnection)
+
+                'get all translations for the specified form and language
+                clsCommand.CommandText = "SELECT control_name, translation FROM form_controls, translations WHERE form_name = """ & ctrParent.Name &
+                                         """ AND language_code = """ & strLanguage & """ And form_controls.id_text = translations.id_text"
+                Using clsReader As SQLiteDataReader = clsCommand.ExecuteReader()
+
+                    'for each translation row
+                    While (clsReader.Read())
+
+                        'ignore rows where the translation text is null or missing
+                        If clsReader.FieldCount < 2 OrElse clsReader.IsDBNull(1) Then
+                            Continue While
+                        End If
+
+                        'translate the menu item's text to the new language
+                        Dim strMenuItemName As String = clsReader.GetString(0)
+                        Dim strTranslation As String = clsReader.GetString(1)
+                        Dim mnuItem As ToolStripMenuItem = Nothing
+                        If dctMenuItems.TryGetValue(strMenuItemName, mnuItem) Then
+                            mnuItem.Text = strTranslation
+                        End If
+
+                    End While
+                End Using
+            End Using
+            clsConnection.Close()
+        End Using
+    End Sub
+
+    Private Shared Function GetDctMenuItems(tsCollection As ToolStripItemCollection) As Dictionary(Of String, ToolStripMenuItem)
+        Dim dctMenuItems As Dictionary(Of String, ToolStripMenuItem) = New Dictionary(Of String, ToolStripMenuItem)
+
+        For Each tsItem As ToolStripItem In tsCollection
+            If Not String.IsNullOrEmpty(tsItem.Text) Then
+                dctMenuItems.Add(tsItem.Name, tsItem)
+            End If
+            Dim mnuItem As ToolStripMenuItem = TryCast(tsItem, ToolStripMenuItem)
+            If mnuItem IsNot Nothing AndAlso mnuItem.HasDropDownItems Then
+                Dim dctSubMenuItems As Dictionary(Of String, ToolStripMenuItem) = GetDctMenuItems(mnuItem.DropDownItems)
+                dctMenuItems = dctMenuItems.Union(dctSubMenuItems).ToDictionary(Function(p) p.Key, Function(p) p.Value)
+            End If
+        Next
+
+        Return dctMenuItems
+    End Function
+
+End Class
